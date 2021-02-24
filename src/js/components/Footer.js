b902f049f002b6f6442c3f8ef16b13561ecb2043
libangyao <libangyao@youzan.com>
2021-02-24 15:40:52 +0800

    feat: 添加一些样式

---
+++ src/js/components/Footer.js
@@ -0,0 +1,17 @@
+export default {
+  template: `
+  <div class="my-footer">
+    <ul>
+      <li>
+        <a href="/docs/" target="_blank">文档</a>
+      </li>
+      <li>
+        <a href="//2021.tomluvjerry.cn" target="_blank">主页</a>
+      </li>
+      <li>
+        <a href="//github.com/lhyUnited" target="_blank">github</a>
+      </li>
+    </ul>
+  </div>
+  `
+}
