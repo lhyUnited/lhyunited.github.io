7eb5e76cd8d1065c84df22673a2b90de514e3060
李帮耀 <lhy_1314@yeah.net>
2022-10-26 21:11:45 +0800

    chore: deprecated link & feat: add new home icon

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -6,10 +6,10 @@ export default {
         <a href="/projects/" target="_blank">项目</a>
       </li>
       <li>
-        <a href="//2021.tomluvjerry.cn/blog" target="_blank">博客</a>
+        <a class="deprecated" href="//2021.tomluvjerry.cn/blog" target="_blank">博客</a>
       </li>
       <li>
-        <a href="//2021.tomluvjerry.cn" target="_blank">主页</a>
+        <a class="deprecated" href="//2021.tomluvjerry.cn" target="_blank">主页</a>
       </li>
       <li>
         <a href="//github.com/lhyUnited/lhyunited.github.io" target="_blank">github</a>
